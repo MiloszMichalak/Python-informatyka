51d15e7ff702b905e12c95a5179bda705b238d2b
MiloszMichalak <miloszmichalak150@gmail.com>
2024-05-27 16:43:29 +0200

    Dodanie uzupelnionego pliku

--- 2 Klasa/PHP/Matura1/index.php
+++ 2 Klasa/PHP/Matura1/index.php
@@ -7,14 +7,27 @@ $baza = "taryfikator";
 
 $connection = new mysqli($server, $user, $password, $baza);
 
-$zapytanie = "";
+$wynik = $connection->query("SELECT kierowcy_txt.Imie, kierowcy_txt.Nazwisko, SUM(taryfikator_txt.Kwota) as Suma
+FROM kierowcy_txt 
+INNER JOIN rejestr_txt ON kierowcy_txt.IdOsoby = rejestr_txt.IdOsoby
+INNER JOIN taryfikator_txt ON taryfikator_txt.IdWykroczenia = rejestr_txt.IdWykroczenia
+GROUP BY kierowcy_txt.IdOsoby ORDER BY Suma DESC LIMIT 1");
 
-if ($wynik = $connection -> query($zapytanie)) {
-    while ($row = $wynik -> fetch_array()) {
-        echo $row["kierowcy_txt"];
-    }
-} else {
-    echo $connection -> error." ".$connection -> errno;
+echo "Zadanie1"."<br>";
+while ($row = $wynik -> fetch_array()) {
+    echo $row["Imie"]." ".$row["Nazwisko"]." ".$row["Suma"]."<br>";
+}
+
+$wynik = $connection->query("SELECT MONTH(rejestr_txt.Data) as Miesiac, Sum(taryfikator_txt.Punkty) as Punkty
+FROM rejestr_txt 
+INNER JOIN taryfikator_txt ON rejestr_txt.IdWykroczenia = taryfikator_txt.IdWykroczenia AND taryfikator_txt.IdWykroczenia BETWEEN 3 AND 6
+GROUP BY Miesiac 
+ORDER BY Punkty DESC
+LIMIT 1;");
+
+echo "Zadanie2"."<br>";
+while ($row = $wynik -> fetch_array()) {
+    echo $row["Miesiac"]." ".$row["Punkty"]."<br>";
 }
 
 $connection->close();
