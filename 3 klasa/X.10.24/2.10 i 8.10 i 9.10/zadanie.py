f7b888623ec0e8071b16b23758f1d6d4b3ec82ee
miloszmichalak <miloszmichalak150@gmail.com>
2024-10-09 15:08:50 +0200

    Dodanie zadania z lekcji 9.10

--- 3 klasa/X.10.24/2.10 i 8.10 i 9.10/zadanie.py
+++ 3 klasa/X.10.24/2.10 i 8.10 i 9.10/zadanie.py
@@ -1,5 +1,4 @@
-from operator import index, imatmul
-
+from random import randint
 
 def SzukanieBinarne(T, a, n):
     left = 0
@@ -29,41 +28,61 @@ else:
         print("Tak")
     else:
         print("Nie")
-        
+
 print("Zadanie 2")
-def SzukanieBinarneReku(T, a, n):
-    left = 0
-    right = n - 1
+def SzukanieBinarneReku(T, a, left, right):
     while left < right:
         middle = (left + right) // 2
         if T[middle] < a:
-            left = middle + 1
+            return SzukanieBinarneReku(T, a, middle+1, right)
         else:
-            right = middle
+            return SzukanieBinarneReku(T, a, left, middle)
     return T[left] == a
 
 
 print("Zadanie 3")
-file = open("ciagi.txt")
+file = open("ciagi.txt", "r")
 k = 1
 for line in file:
     numbers = list(map(int, line.split()))
-    if SzukanieBinarneReku(numbers, 10, len(numbers)):
+    if SzukanieBinarneReku(numbers, 10, 0, len(numbers) - 1):
         print(f"{k}: {numbers}" )
     k += 1
     
 file.close()
 
 print("Zadanie 4")
-with open("ciagi.txt") as f:
-    a = 10
-    n = int(file.readline())
-    index = 1
+file = open("ciagi2.txt", "r")
+n = int(file.readline().rstrip())
+for i in range(n):
+    n = int(file.readline().rstrip())
     numbers = list(map(int, file.readline().split()))
-    if SzukanieBinarne(numbers, a, n):
-        print(f"{index}: {numbers}" )
-        index += 1
-    
+    if SzukanieBinarne(numbers, 10, n):
+        print(f"{i + 1}: {numbers}" )
+        
+file.close()
 
 # zadanie 5
 print("Zadanie 5")
+# pseudokod
+
+
+# zadanie 6
+print("Zadanie 6")
+numbers = []
+n = randint(1, 10)
+numbers.append(n)
+for i in range(1_000_000):
+    n += randint(1, 3)
+    numbers.append(n)
+    
+file = open("zadanie4.txt", "w")
+if SzukanieBinarne(numbers, 1_500_000, len(numbers)):
+    file.write("TAK")
+else:
+    file.write("NIE")
+file.close()
+
+# zadanie7 - maturalne
+print("Zadanie 7 - maturalne")
+
