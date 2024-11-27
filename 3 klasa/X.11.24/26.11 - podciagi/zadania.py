214be2bdba82098a5c762577d460896fcaf590b5
miloszmichalak <miloszmichalak150@gmail.com>
2024-11-27 15:06:36 +0100

    Dodanie 2 zadan z podciagami

--- 3 klasa/X.11.24/26.11 - podciagi/zadania.py
+++ 3 klasa/X.11.24/26.11 - podciagi/zadania.py
@@ -1,31 +1,63 @@
 # Zadanie 1
-from traceback import print_tb
-from turtledemo.penrose import start
+from ipaddress import summarize_address_range
+from itertools import count
+from turtledemo.chaos import coosys
 
 print("Zadanie 1")
 
 file = open("liczby.txt")
-# ciag = list(map(int, file.readline().split()))
-ciag = [3, 2, 5, 1]
-
+ciag = list(map(int, file.readline().split()))
 itemsNumber = len(ciag)
-#moj sposob
-def sposobTrudny(itemsNumber, n = 1, start = 0):
-    for i in range(itemsNumber):
-        for j in range(itemsNumber):
-            if start + n <= itemsNumber:
-                print(ciag[start:start + n])
-            start += 1
-    n += 1
-    start = 0
     
+# sposobu mojego nie ma
     
 #sposob prosty
 n = len(ciag)
 def prostySposob(n):
-    for len in range(1, n+1):
-        for start in range(n-len+1):
-            print(*ciag[start:start+len])
+    for len in range(1, n + 1):
+        for start in range(n - len + 1):
+            print(*ciag[start:start + len])
             
-sposobTrudny(len(ciag))
+            
 prostySposob(len(ciag))
+
+file.close()
+
+
+# Zadanie 2
+print("Zadanie 2")
+file = open("liczby.txt")
+ciag = list(map(int, file.readline().split()))
+itemsNumber = len(ciag)
+        
+def sposobProsty(ciag):
+    for i in range(itemsNumber):
+        for j in range(i, itemsNumber):
+            print(*ciag[i:j + 1])
+        
+        
+sposobProsty(ciag)
+        
+file.close()
+
+# Zadanie 3
+print("Zadanie 3")
+file = open("liczby.txt")
+ciag = list(map(int, file.readline().split()))
+
+
+for i in range(len(ciag) - 4):
+    podciag = ciag[i:i + 5]
+    suma = sum(podciag)
+    print(podciag, suma)
+    
+file.close()
+
+# Zadanie 4
+print("Zadanie 4")
+file = open("liczby.txt")
+ciag = list(map(int, file.readline().split()))
+
+# todo
+
+
