998c02aada6f9559ef5f722afe48ea0b88e2edf7
miloszmichalak <miloszmichalak150@gmail.com>
2026-09-15 07:22:29 +0200

    Usuniete zbedne pliki

---
+++ 4 Klasa/X.05.26/Zadanie5Informator/zadanie5.4.py
@@ -0,0 +1,16 @@
+﻿A = [0, 2, 4, 6, 8, 10, 9, 7, 5, 3, 1]
+
+def W(j):
+    v = A[j]
+    i = j
+    while i > 1 and A[i - 1] > v:
+        A[i] = A[i - 1]
+        i -= 1
+    A[i] = v
+
+print(A)
+W(7)
+print(A)
+
+W(9)
+print(A)
---
+++ 4 Klasa/X.05.26/Zadanie5Informator/zadanie5.py
@@ -0,0 +1,14 @@
+﻿A = [0,2,4,6,8,10,9,7,5,3,1]
+
+def W(j):
+    if j > 1:
+        if A[j] < A[j - 1]:
+            A[j], A[j - 1] = A[j - 1], A[j]
+            W(j - 1)
+
+print(A)
+W(7)
+print(A)
+
+W(9)
+print(A)
